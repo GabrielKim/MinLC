5cf58034a983c7a32587ff5afb4f959560640add
Doohoon Kim <invi.dh.kim@gmail.com>
2017-02-16 00:20:29 +0900

    Revise the mlc.base.def of wrong function names.

--- src/base/Def/mlc.base.def.hpp
+++ src/base/Def/mlc.base.def.hpp
@@ -68,14 +68,14 @@
 #elif defined __GNUC__
   #if defined __clang__ && __clang_major__ >= 3 && !defined __ANDROID__ && !defined __EMSCRIPTEN__ && !defined(__CUDACC__)
     #ifdef __ATOMIC_ACQ_REL
-      #define MLC_FETCH_ADD(addr, delta) __c11_atomic_MLC_FETCH_ADD((_Atomic(int)*)(addr), delta, __ATOMIC_ACQ_REL)
+      #define MLC_FETCH_ADD(addr, delta) __c11_atomic_fetch_add((_Atomic(int)*)(addr), delta, __ATOMIC_ACQ_REL)
     #else
-      #define MLC_FETCH_ADD(addr, delta) __atomic_MLC_FETCH_ADD((_Atomic(int)*)(addr), delta, 4)
+      #define MLC_FETCH_ADD(addr, delta) __atomic_fetch_add((_Atomic(int)*)(addr), delta, 4)
     #endif
   #else
     #if defined __ATOMIC_ACQ_REL && !defined __clang__
        // version for gcc >= 4.7
-      #define MLC_FETCH_ADD(addr, delta) (int)__atomic_MLC_FETCH_ADD((unsigned*)(addr), (unsigned)(delta), __ATOMIC_ACQ_REL)
+      #define MLC_FETCH_ADD(addr, delta) (int)__atomic_fetch_add((unsigned*)(addr), (unsigned)(delta), __ATOMIC_ACQ_REL)
     #else
       #define MLC_FETCH_ADD(addr, delta) (int)__sync_fetch_and_add((unsigned*)(addr), (unsigned)(delta))
     #endif
